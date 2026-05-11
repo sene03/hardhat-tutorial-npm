72541b74e3b5fc2d2c88ad90d39fceaad5353fd9
sene03 <hansel0311@gmail.com>
2026-05-11 15:07:35 +0900

    make gas fee zero

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,6 +7,7 @@ const config: HardhatUserConfig = {
     besu: {
       url: "http://localhost:8545",
       chainId: 1337,
+      gasPrice: 0,
       accounts: [
         "8f2a55949038a9610f50fb23b5883af3b4ecb3c3bb792cbcefbd1542c692be63",
         "c87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3",
